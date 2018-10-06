e978d4b5c212db6f7dec27efbad49886ac637cd6
Rasmus <Rasmus.grinsvall@elev.ga.lbs.se>
2018-10-06 03:30:15 +0200

    Kontroller
    Kontroller är nu klara och fungerande för e nivå saknas färg

--- Assets/Shipcontrolls.cs
+++ Assets/Shipcontrolls.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Shipcontrolls : MonoBehaviour
 {
+    public float Hastighetframmåt = 10;
+    public float Svänghastighet = 180;
 
     // Use this for initialization
     void Start()
@@ -15,6 +17,16 @@ public class Shipcontrolls : MonoBehaviour
     void Update()
     {
 
-        transform.Translate(0, -10f * Time.deltaTime, 0, Space.Self);
+        transform.Translate(0, -Hastighetframmåt * Time.deltaTime, 0, Space.Self); //gör så att du åker konstant frammåt
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(0, Hastighetframmåt / 2 * Time.deltaTime, 0, Space.Self); //gör så att du åker i hälften av hastigheten frammåt
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(0, 0, -Svänghastighet / 2 * Time.deltaTime); //gör så att skeppet svänger vänster lite långsammare än åt höger //if (Input.GetKey(KeyCode.D)) transform.Rotate(0, 0, Svänghastighet-60 * Time.deltaTime); ha inte såhär det händer något konstigt
+
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(0, 0, Svänghastighet * Time.deltaTime);  //gör så att skepet svänger höger 
+
     }
 }
